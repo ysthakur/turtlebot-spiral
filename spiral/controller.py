341cee49874bbdb98ad7b5d4d3f5ec69b97d848d
Yash Thakur <2004yst@gmail.com>
2024-02-16 18:11:43 -0500

    Initial commit

---
+++ spiral/controller.py
@@ -0,0 +1,40 @@
+import rclpy
+from rclpy.node import Node
+
+from std_msgs.msg import Twist
+from geometry_msgs.msg import Vector3
+
+
+class Controller(Node):
+
+    def __init__(self):
+        super().__init__('spiral_controller')
+        self.publisher_ = self.create_publisher(Twist, 'topic', 10)
+        timer_period = 0.5  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.i = 0
+
+    def timer_callback(self):
+        msg = Twist()
+        msg.data = 'Hello World: %d' % self.i
+        self.publisher_.publish(msg)
+        self.get_logger().info('Publishing: "%s"' % msg.data)
+        self.i += 1
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    controller = Controller()
+
+    rclpy.spin(controller)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    controller.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
