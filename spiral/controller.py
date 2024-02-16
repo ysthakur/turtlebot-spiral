e6d38e13beca260f26ff5947763366f5acd5a23a
Yash Thakur <2004yst@gmail.com>
2024-02-16 18:26:46 -0500

    Make it go in a circle

--- spiral/controller.py
+++ spiral/controller.py
@@ -1,25 +1,21 @@
 import rclpy
 from rclpy.node import Node
 
-from std_msgs.msg import Twist
-from geometry_msgs.msg import Vector3
+from geometry_msgs.msg import Twist, Vector3
 
 
 class Controller(Node):
 
     def __init__(self):
-        super().__init__('spiral_controller')
-        self.publisher_ = self.create_publisher(Twist, 'topic', 10)
-        timer_period = 0.5  # seconds
+        super().__init__("spiral_controller")
+        self.publisher_ = self.create_publisher(Twist, "spiral/cmd_vel", 10)
+        timer_period = 1 # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.i = 0
 
     def timer_callback(self):
-        msg = Twist()
-        msg.data = 'Hello World: %d' % self.i
+        msg = Twist(linear=Vector3(x=1.0, y=1.0, z=1.0), angular=Vector3(x=1.0, y=1.0, z=1.0))
         self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
-        self.i += 1
+        # self.get_logger().info('Publishing: "%s"' % msg)
 
 
 def main(args=None):
@@ -36,5 +32,5 @@ def main(args=None):
     rclpy.shutdown()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
