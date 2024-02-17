265f10aa2b50277d495e8d1627d76cee3627c8aa
Yash Thakur <2004yst@gmail.com>
2024-02-17 13:04:22 -0500

    Spiraling out of control

--- spiral/controller.py
+++ spiral/controller.py
@@ -9,12 +9,14 @@ class Controller(Node):
     def __init__(self):
         super().__init__("spiral_controller")
         self.publisher_ = self.create_publisher(Twist, "spiral/cmd_vel", 10)
-        timer_period = 1 # seconds
+        timer_period = 0.5 # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.theta = 2.0
 
     def timer_callback(self):
-        msg = Twist(linear=Vector3(x=1.0, y=1.0, z=1.0), angular=Vector3(x=1.0, y=1.0, z=1.0))
+        msg = Twist(linear=Vector3(x=0.5, y=0.5, z=0.0), angular=Vector3(x=0.0, y=0.0, z=self.theta))
         self.publisher_.publish(msg)
+        self.theta *= 0.95
         # self.get_logger().info('Publishing: "%s"' % msg)
 
 
